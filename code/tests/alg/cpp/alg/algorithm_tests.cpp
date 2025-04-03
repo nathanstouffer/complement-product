bdf79d6645e2f50ceb98ff83292566a6634a14f4
Nathan Stouffer <nathanstouffer1999@gmail.com>
2025-04-02 21:16:15 -0600

    add more test cases

--- code/tests/alg/cpp/alg/algorithm_tests.cpp
+++ code/tests/alg/cpp/alg/algorithm_tests.cpp
@@ -33,6 +33,8 @@ namespace alg
         }
     }
 
+    inline stff::mtx2 rot(float theta) { return stf::math::rotate<float>(theta); }
+
     TEST(complement_product, matrices)
     {
         std::vector<scaffolding::matrices> tests = 
@@ -41,13 +43,11 @@ namespace alg
             { { stff::mtx2() }, { stff::mtx2() } },
             { { stff::mtx2(4) }, { stff::mtx2() } },
             // test cases with two values should swap positions
-            //{ { 1, 0 }, { 0, 1 }},
-            //// test cases with a single zero should only have on product that matters
-            //{ { 0, 1, 2 }, { 2, 0, 0 }},
-            //// test cases with two zeros should return all zeros
-            //{ { 0, 6, 2, 0 }, { 0, 0, 0, 0 }},
-            //// general test cases
-            //{ { 1, 6, 2, 5 }, { 60, 10, 30, 12 }},
+            { { stff::mtx2(1), stff::mtx2() }, { stff::mtx2(), stff::mtx2(1) } },
+            { { stff::mtx2(5), stff::mtx2(1) }, { stff::mtx2(1), stff::mtx2(5) } },
+            { { stff::mtx2(stff::vec4(1, 0, 0, 5)), rot(3) }, { rot(3), stff::mtx2(stff::vec4(1, 0, 0, 5)) } },
+            // general test cases
+            { { rot(1), rot(2), rot(3), rot(4) }, { rot(9), rot(8), rot(7), rot(6) }},
         };
 
         std::size_t i = 0;
