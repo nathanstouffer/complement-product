2baa3e1573ef184b5e728bf94952cae2b800adf3
Nathan Stouffer <nathanstouffer1999@gmail.com>
2025-04-02 21:04:55 -0600

    add test cases for matrices

--- code/tests/alg/cpp/alg/algorithm_tests.cpp
+++ code/tests/alg/cpp/alg/algorithm_tests.cpp
@@ -33,4 +33,28 @@ namespace alg
         }
     }
 
+    TEST(complement_product, matrices)
+    {
+        std::vector<scaffolding::matrices> tests = 
+        {
+            // test cases where there is just one value => return the identity matrix
+            { { stff::mtx2() }, { stff::mtx2() } },
+            { { stff::mtx2(4) }, { stff::mtx2() } },
+            // test cases with two values should swap positions
+            //{ { 1, 0 }, { 0, 1 }},
+            //// test cases with a single zero should only have on product that matters
+            //{ { 0, 1, 2 }, { 2, 0, 0 }},
+            //// test cases with two zeros should return all zeros
+            //{ { 0, 6, 2, 0 }, { 0, 0, 0, 0 }},
+            //// general test cases
+            //{ { 1, 6, 2, 5 }, { 60, 10, 30, 12 }},
+        };
+
+        std::size_t i = 0;
+        for (scaffolding::matrices const& test : tests)
+        {
+            test.verify(i++);
+        }
+    }
+
 }
