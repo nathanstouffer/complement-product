39f553dfb4cf6bbb5fe11430b4a482f2b3fbbf12
Nathan Stouffer <nathanstouffer1999@gmail.com>
2025-04-02 20:11:42 -0600

    set up testing for library

---
+++ code/tests/alg/include/private/alg/scaffolding/algorithm.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include <alg/algorithm.hpp>
+
+namespace alg::scaffolding
+{
+
+    struct numbers
+    {
+        std::vector<float> input;
+        std::vector<float> expected;
+
+        void verify(size_t const i) const
+        {
+            std::vector<float> result = complement_product(input);
+            ASSERT_EQ(expected, result) << "Failed to correctly compute " << i << "th complement product";
+        }
+
+    };
+
+} // alg::scaffolding
